84b71d83191743d2170b2b53be2919af0a9bbe52
Eappahjen1 <eappahjen@gmail.com>
2023-02-17 08:47:50 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,31 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include<stdio.h>
 
 /**
- * main - Determines either greater than 5, is less than 6, or is 0
+ * main - prints the alphabet in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	int ld;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	ld = n % 10;
-	if (ld > 5)
+	for (n = 'a'; n <= 'z'; n++)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+		putchar(n);
 	}
-	else if (ld == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, ld);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
-	}
+	putchar('\n');
+
 	return (0);
 }
